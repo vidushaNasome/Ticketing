3aafd30a76cbb4fa0d5a26522c1bda26d655d075
vidushaNasome <vnasome58@gmail.com>
2020-10-15 10:08:40 +0530

    commit sml chngs

---
+++ app/src/test/java/com/example/ticket/StartActivityTest.java
@@ -0,0 +1,30 @@
+package com.example.ticket;
+
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(JUnit4.class)
+public class StartActivityTest {
+
+    @Rule
+
+
+    @Test
+    public void onCreate() {
+
+    }
+
+    @Test
+    public void onResume() {
+
+
+    }
+}
