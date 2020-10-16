b19b4bfae55d152fc438d779b5114f7a9eed7f73
vidushaNasome <vnasome58@gmail.com>
2020-10-16 15:15:16 +0530

    commit sml chngs12

--- app/src/androidTest/java/com/example/ticket/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ticket/ExampleInstrumentedTest.java
@@ -1,13 +1,23 @@
 package com.example.ticket;
 
 import android.content.Context;
+import android.content.Intent;
+import android.renderscript.Sampler;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.notNullValue;
 
-
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
 import static org.junit.Assert.*;
 
 /**
@@ -17,11 +27,113 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public final ActivityTestRule<StartActivity> rule = new ActivityTestRule<>(StartActivity.class, true);
+
+    @Rule
+    public ActivityTestRule<Ticketing> rule2  = new  ActivityTestRule<Ticketing>(Ticketing.class)
+    {
+        @Override
+        protected Intent getActivityIntent() {
+            Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.putExtra("id", "998353719V");
+            return intent;
+        }
+    };
+
+    @Rule
+    public ActivityTestRule<TicketPurchaseActivity> rule3  = new  ActivityTestRule<TicketPurchaseActivity>(TicketPurchaseActivity.class)
+    {
+        @Override
+        protected Intent getActivityIntent() {
+            Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.putExtra("details", "hhhhhhhhh-90");
+            intent.putExtra("un", "998353719V");
+            return intent;
+        }
+    };
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.ticket", appContext.getPackageName());
+    }
+
+    //Detecting whether fields are empty
+
+    //class StartActivity
+    @Test
+    public void validateFieldsStartActivity(){
+        StartActivity activity = rule.getActivity();
+        View viewById1 = activity.findViewById(R.id.busname);
+        assertThat(viewById1,notNullValue());
+
+        View viewById2 = activity.findViewById(R.id.busname);
+        assertThat(viewById2,notNullValue());
+
+        //View viewById3 = activity.findViewById(R.id.bussubmit);
+
+
+
+    }
+    //class Ticketing
+    @Test
+    public void ensureIntentDataIsDisplayed() throws Exception {
+        Ticketing activity = rule2.getActivity();
+
+        View viewById = activity.findViewById(R.id.unid);
+        View viewById1 = activity.findViewById(R.id.emailid);
+        View viewById2 = activity.findViewById(R.id.creditsId);
+
+        assertThat(viewById,notNullValue());
+        assertThat(viewById, instanceOf(TextView.class));
+        TextView textView = (TextView) viewById;
+        assertThat(textView.getText().toString(),is("998353719V"));
+
+        assertThat(viewById1,notNullValue());
+        assertThat(viewById1, instanceOf(TextView.class));
+        TextView textView1 = (TextView) viewById1;
+        assertThat(textView1.getText().toString(),is("shashi@gmail.com"));
+
+        assertThat(viewById2,notNullValue());
+        assertThat(viewById2, instanceOf(TextView.class));
+        TextView textView2 = (TextView) viewById2;
+        assertThat(textView2.getText().toString(),is("200"));
+
+
+
+    }
+    //class TicketPurchaseActivity
+    @Test
+    public void ensureIntentDataIsDisplayedInTicketing2() throws Exception {
+        TicketPurchaseActivity activity = rule3.getActivity();
+
+        View viewById = activity.findViewById(R.id.unid);
+        View viewById1 = activity.findViewById(R.id.emailid);
+        View viewById2 = activity.findViewById(R.id.creditsId);
+
+        assertThat(viewById,notNullValue());
+        assertThat(viewById, instanceOf(TextView.class));
+        TextView textView = (TextView) viewById;
+        assertThat(textView.getText().toString(),is("998353719V"));
+
+        assertThat(viewById1,notNullValue());
+        assertThat(viewById1, instanceOf(TextView.class));
+        TextView textView1 = (TextView) viewById1;
+        assertThat(textView1.getText().toString(),is("shashi@gmail.com"));
+
+        assertThat(viewById2,notNullValue());
+        assertThat(viewById2, instanceOf(TextView.class));
+        TextView textView2 = (TextView) viewById2;
+        assertThat(textView2.getText().toString(),is("200"));
+
+
+
     }
 }
